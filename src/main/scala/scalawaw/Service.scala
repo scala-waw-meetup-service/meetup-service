46d09172d9bfd62b208c66236d163a2e1be498ec
Bartek Kalinka <bartkali@hotmail.com>
2016-02-04 19:12:32 +0100

    refactoring 1

---
+++ src/main/scala/scalawaw/Service.scala
@@ -0,0 +1,45 @@
+package scalawaw
+
+import akka.actor.ActorSystem
+import akka.http.scaladsl.HttpExt
+import akka.http.scaladsl.model.HttpRequest
+import akka.stream.ActorMaterializer
+import akka.util.ByteString
+import spray.json._
+
+import scala.concurrent.Await
+import scala.concurrent.duration._
+
+case class MeetupGroup(id: Long, name: String)
+
+object MyJsonProtocol extends DefaultJsonProtocol {
+  implicit val colorFormat = jsonFormat2(MeetupGroup)
+}
+
+object Const {
+  val meetupUrl = "https://api.meetup.com/find"
+  val timeout = 70 seconds
+}
+
+case class Service(http: HttpExt, apiKey: String) {
+  import MyJsonProtocol._
+
+  private def tokenParam: String = s"?key=$apiKey"
+
+  private def urlToJsonAst(inputUrl: String): JsValue = {
+    implicit val system = ActorSystem()
+    implicit val materializer = ActorMaterializer()
+    val response = Await.result(http.singleRequest(HttpRequest(uri = inputUrl)), Const.timeout)
+    println(s"status ${response.status}")
+    val responseContent = Await.result(response.entity.dataBytes.runFold(ByteString(""))(_ ++ _), Const.timeout)
+    val responseStr = responseContent.decodeString("UTF8")
+    println(s"resp content ${responseStr.substring(0, 100)}")
+    responseStr.parseJson
+  }
+
+  private def buildUrl(specificPart: String) = s"${Const.meetupUrl}/$specificPart$tokenParam"
+
+  def listGroups: List[MeetupGroup] = {
+    urlToJsonAst(buildUrl("groups")).convertTo[List[MeetupGroup]]
+  }
+}
