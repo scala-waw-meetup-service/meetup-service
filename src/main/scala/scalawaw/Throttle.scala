ad83257660f960478a00089b24ca5e0d8d267153
bartlomiej.kalinka <bartlomiej.kalinka@javeo.eu>
2016-03-02 16:36:44 +0100

    zipWith throttling works

--- src/main/scala/scalawaw/Throttle.scala
+++ src/main/scala/scalawaw/Throttle.scala
@@ -1,21 +1,22 @@
 package scalawaw
 
+import akka.NotUsed
 import akka.actor.Cancellable
 import akka.stream._
 import akka.stream.scaladsl.{ZipWith, GraphDSL, Source}
 import scala.concurrent.duration._
 
 object Throttle {
-  private def mainTick: Source[Unit, Cancellable] = Source.tick(0 seconds, 1 seconds, ())
+  private def mainTick: Source[Unit, Cancellable] = Source.tick(0 seconds, 5 seconds, ())
   
-  private def zipWithNode[A](implicit builder: GraphDSL.Builder[Unit]): FanInShape2[Unit, A, A] =  {
+  private def zipWithNode[A](implicit builder: GraphDSL.Builder[NotUsed]): FanInShape2[Unit, A, A] =  {
     val zipWith = ZipWith[Unit, A, A]((a: Unit, i: A) => i)
     val zipWithSmallBuffer = zipWith.withAttributes(Attributes.inputBuffer(initial = 1, max = 1))
     builder.add(zipWithSmallBuffer)
   }
 
-  def zipWithThrottleTick[A, B](toZip: Source[A, Unit]): Source[A, Unit] =
-    Source.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[Unit] =>
+  def zipWithThrottleTick[A, B](toZip: Source[A, Unit]): Source[A, NotUsed] =
+    Source.fromGraph(GraphDSL.create() { implicit builder: GraphDSL.Builder[NotUsed] =>
       import GraphDSL.Implicits._
       val zipNode = zipWithNode[A]
       mainTick ~> zipNode.in0
